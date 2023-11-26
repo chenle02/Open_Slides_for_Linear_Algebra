14e7091576acb59b553a09b1d33fd97c8a75135a
chenle02 <chenle02@gmail.com>
2023-11-26 00:26:38 -0500

    Update the topics

---
+++ myzsh.zsh
@@ -0,0 +1,9 @@
+
+alias description="gh repo edit \
+  --description 'Open Slides for Linear Albegra' \
+  --homepage 'https://doi.org/10.5281/zenodo.10206020' \
+  --add-topic 'teaching' \
+  --add-topic 'slides' \
+  --add-topic 'linear-algebra' \
+  --add-topic 'latex' \
+  "
